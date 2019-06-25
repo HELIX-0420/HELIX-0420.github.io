aa708b1d76f8ab4f90d9a670df0a085ab2a40ec5
chelsea <jordanbell213@yahoo.co.uk>
2019-06-25 14:00:41 -0700

    test

--- accets/js/custom.js
+++ accets/js/custom.js
@@ -3,11 +3,11 @@ var dark = false;
 document.querySelector(".switch").addEventListener("click", function() {
     if (dark === true) {
         document.querySelector(".theme").setAttribute("href", "accets/css/main-lite.css");
-    document.querySelector(".switch").innerHTML = "🌑";
+        document.querySelector(".switch").innerHTML = "🌑";
         dark = false;
     } else {
         document.querySelector(".theme").setAttribute("href", "accets/css/main-dark.css");
-    document.querySelector(".switch").innerHTML = "☀";
+        document.querySelector(".switch").innerHTML = "☀";
         dark = true;
     }
 });
