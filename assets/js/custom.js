e7ca89dec18619cfb8fdc1575a060676ace841fd
chelsea <jordanbell213@yahoo.co.uk>
2019-06-26 05:07:39 -0700

    test

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,11 +2,11 @@ var dark = false;
 
 document.querySelector(".switch").addEventListener("click", function() {
     if (dark === true) {
-        document.querySelector(".theme").setAttribute("href", "accets/css/main-lite.css");
+        document.querySelector(".theme").setAttribute("href", "assets/css/main-lite.css");
         document.querySelector(".switch").innerHTML = "🌑";
         dark = false;
     } else {
-        document.querySelector(".theme").setAttribute("href", "accets/css/main-dark.css");
+        document.querySelector(".theme").setAttribute("href", "assets/css/main-dark.css");
         document.querySelector(".switch").innerHTML = "🌞";
         dark = true;
     }
